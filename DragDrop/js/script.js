384550ac9552bf33a66dbf5205a4d1bbd0266a5a
vmorenma <victor.moreno.maya@gmail.com>
2017-03-14 20:39:31 +0100

    DragDrop

---
+++ DragDrop/js/script.js
@@ -0,0 +1,16 @@
+$( init );
+ 
+function init() {
+  $('#makeMeDraggable').draggable({containment:"#content"},{axis:'x'},{snap:'#makeMeDroppable'});
+  $('#makeMeDroppable').droppable( 
+    
+    {
+        drop: handleDropEvent
+    }
+   );
+}
+ 
+function handleDropEvent( event, ui ) {
+  window.location.href = 'http://www.example.com';
+}
+ 
