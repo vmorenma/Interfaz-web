225fa6389f72ff3cdf9d528950d8a95e4aeea38b
vmorenma <victor.moreno.maya@gmail.com>
2017-03-16 20:36:21 +0100

    Better style

--- DragDrop/js/script.js
+++ DragDrop/js/script.js
@@ -1,16 +1,24 @@
-$( init );
- 
+$(init);
+
 function init() {
-  $('#makeMeDraggable').draggable({containment:"#content"},{axis:'x'},{snap:'#makeMeDroppable'});
-  $('#makeMeDroppable').droppable( 
-    
-    {
-        drop: handleDropEvent
-    }
-   );
+    $('#makeMeDraggable').draggable(
+        {
+            containment: "#content"
+        },
+        {
+            axis: 'x'
+        },
+        {
+            snap: '#makeMeDroppable'
+        }
+    );
+    $('#makeMeDroppable').droppable(
+        {
+            drop: handleDropEvent
+        }
+    );
 }
- 
-function handleDropEvent( event, ui ) {
-  window.location.href = 'http://www.example.com';
+
+function handleDropEvent(event, ui) {
+    window.location.href = 'http://www.example.com';
 }
- 
